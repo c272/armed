ae5fd5119244db3609cdfe0d207e4d2a22ceeb09
C272 <realrussianmanconfirmed@gmail.com>
2020-12-09 15:26:38 +0000

    Add compiler output and project management panels.

--- Editor.Designer.cs
+++ Editor.Designer.cs
@@ -38,12 +38,29 @@
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.undoButton = new System.Windows.Forms.ToolStripButton();
             this.redoButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.runButton = new System.Windows.Forms.ToolStripButton();
             this.tabs = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.scintilla1 = new ScintillaNET.Scintilla();
+            this.splitter = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.splitterParent = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.projectLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.toolStrip1.SuspendLayout();
             this.tabs.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitter)).BeginInit();
+            this.splitter.Panel1.SuspendLayout();
+            this.splitter.Panel2.SuspendLayout();
+            this.splitter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitterParent)).BeginInit();
+            this.splitterParent.Panel1.SuspendLayout();
+            this.splitterParent.Panel2.SuspendLayout();
+            this.splitterParent.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
@@ -57,7 +74,9 @@
             this.saveAsButton,
             this.toolStripSeparator1,
             this.undoButton,
-            this.redoButton});
+            this.redoButton,
+            this.toolStripSeparator2,
+            this.runButton});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(1058, 27);
@@ -141,16 +160,29 @@
             this.redoButton.Text = "Redo";
             this.redoButton.ToolTipText = "Redo (CTRL+Y)";
             // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 27);
+            // 
+            // runButton
+            // 
+            this.runButton.Image = ((System.Drawing.Image)(resources.GetObject("runButton.Image")));
+            this.runButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(136, 24);
+            this.runButton.Text = "Run and Debug";
+            // 
             // tabs
             // 
             this.tabs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tabs.Controls.Add(this.tabPage1);
-            this.tabs.Location = new System.Drawing.Point(0, 30);
+            this.tabs.Location = new System.Drawing.Point(0, 0);
             this.tabs.Name = "tabs";
             this.tabs.SelectedIndex = 0;
-            this.tabs.Size = new System.Drawing.Size(1058, 595);
+            this.tabs.Size = new System.Drawing.Size(838, 487);
             this.tabs.TabIndex = 2;
             this.tabs.SelectedIndexChanged += new System.EventHandler(this.tabChanged);
             // 
@@ -160,7 +192,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1050, 566);
+            this.tabPage1.Size = new System.Drawing.Size(830, 458);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "newFile.asm";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -170,23 +202,120 @@
             this.scintilla1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.scintilla1.Location = new System.Drawing.Point(3, 3);
             this.scintilla1.Name = "scintilla1";
-            this.scintilla1.Size = new System.Drawing.Size(1044, 560);
+            this.scintilla1.Size = new System.Drawing.Size(824, 452);
             this.scintilla1.TabIndex = 0;
             this.scintilla1.Text = "scintilla1";
+            // 
+            // splitter
+            // 
+            this.splitter.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitter.Location = new System.Drawing.Point(3, 30);
+            this.splitter.Name = "splitter";
+            this.splitter.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitter.Panel1
+            // 
+            this.splitter.Panel1.Controls.Add(this.tabs);
+            // 
+            // splitter.Panel2
+            // 
+            this.splitter.Panel2.Controls.Add(this.label1);
+            this.splitter.Panel2.Controls.Add(this.richTextBox1);
+            this.splitter.Size = new System.Drawing.Size(838, 594);
+            this.splitter.SplitterDistance = 487;
+            this.splitter.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(838, 103);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // splitterParent
+            // 
+            this.splitterParent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitterParent.Location = new System.Drawing.Point(0, 0);
+            this.splitterParent.Name = "splitterParent";
+            // 
+            // splitterParent.Panel1
+            // 
+            this.splitterParent.Panel1.Controls.Add(this.splitter);
+            // 
+            // splitterParent.Panel2
+            // 
+            this.splitterParent.Panel2.Controls.Add(this.projectLbl);
+            this.splitterParent.Panel2.Controls.Add(this.textBox1);
+            this.splitterParent.Panel2.Controls.Add(this.treeView1);
+            this.splitterParent.Size = new System.Drawing.Size(1058, 624);
+            this.splitterParent.SplitterDistance = 844;
+            this.splitterParent.TabIndex = 4;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Location = new System.Drawing.Point(3, 75);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(204, 546);
+            this.treeView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(3, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // projectLbl
+            // 
+            this.projectLbl.AutoSize = true;
+            this.projectLbl.Location = new System.Drawing.Point(1, 31);
+            this.projectLbl.Name = "projectLbl";
+            this.projectLbl.Size = new System.Drawing.Size(112, 17);
+            this.projectLbl.TabIndex = 2;
+            this.projectLbl.Text = "Project Manager";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Output";
             // 
             // Editor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(1058, 624);
-            this.Controls.Add(this.tabs);
             this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.splitterParent);
             this.Name = "Editor";
             this.Text = "Armed Editor";
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.tabs.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.splitter.Panel1.ResumeLayout(false);
+            this.splitter.Panel2.ResumeLayout(false);
+            this.splitter.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitter)).EndInit();
+            this.splitter.ResumeLayout(false);
+            this.splitterParent.Panel1.ResumeLayout(false);
+            this.splitterParent.Panel2.ResumeLayout(false);
+            this.splitterParent.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitterParent)).EndInit();
+            this.splitterParent.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -206,6 +335,15 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripButton undoButton;
         private System.Windows.Forms.ToolStripButton redoButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton runButton;
+        private System.Windows.Forms.SplitContainer splitter;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.SplitContainer splitterParent;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label projectLbl;
     }
 }
 
